2391ffc68a4bc4361b8f4e93d7ab573902e34cf0
datnv6712 <datnv6712@gmail.com>
2021-06-30 15:30:01 +0700

    route where1

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,4 +1,4 @@
 <a href="{{route('user')}}">user</a>
 <a href="product">product</a>
 <a href="/news/1">new 1</a>
-<a href="{{ route ('details',['id'=>2,'cate'=>2] ) }}">new 2 cate 2</a>
+<a href="{{ route ('details',['id'=>12,'cate'=>2] ) }}">new 2 cate 2</a>
